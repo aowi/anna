dce92dc73a510f43f5740ac4e0d368ba6c8b0165
Anders Ossowicki <desu@fys.ku.dk>
2008-01-05 01:32:20 +0100

    add current website

---
+++ website/development.php
@@ -0,0 +1,41 @@
+<div class='main_box'>
+        <div class='main_box_head'><span class='main_head'>Development</span></div>
+        <div class='main_box_content'>
+		<p>The development of Anna^ mainly takes place in irc channel #frokostgruppen on irc.blitzed.org. If you have any suggestions or would like to help out, this is the place to go. Anna^ follows a simple roadmap, that is subject to change at any given time, when it is found nescessary to add, edit or delete something.</p>
+		<p>The first thing to do, if you want to give a hand or use the code, is to get the lates svn snapshot from our <a href='http://sf.net/projects/anna'>SF.net project site</a>. The svn snapshot is pretty much always up-to-date, and a working version of the svn code can be found in #frokostgruppen, under the nickname "Anna^".</p>
+		<p>The versioning of Anna^ uses the x.yz syntax, where z marks a bugfix-release, y marks a bigger release. x means that we went insane and rewrote Anna^ in ada or cobol or something like that.</p>
+        </div><br />
+        <div class='main_box_head'><span class='main_head'>Roadmap</span></div>
+        <div class='main_box_content'>
+		<p>The roadmap (as from the the trunk/TODO file, revision 39):</p>
+		<p>Version 0.40:</p>
+		<ul>
+			<li>Rewrite the bot to use POE and POE::Component::IRC (completed)</li>
+			<li>Basic authentication based on the database</li>
+			<li>Auto-op'ing of users</li>
+			<li>Optionally coloured output to the IRC channel</li>
+		</ul>
+		<p>Version 0.50:</p>
+		<ul>
+			<li>Command-line interface</li>
+			<li>Support for multiple channels</li>
+			<li>Support for multiple networks</li>
+		</ul>
+		<p>Version 0.60:</p>
+		<ul>
+			<li>Modularization</li>
+		</ul>
+		<p>Future features with no specific version attachement:</p>
+		<ul>
+			<li>Bugzilla-watching</li>
+			<li>Regexp-based expander</li>
+			<li>Trivia!!oneoneeleven</li>
+			<li>Math evaluation</li>
+			<li>Babelfish interaction</li>
+			<li>AniDB UDP interface</li>
+			<li>Flood protection</li>
+			<li>Spell checking</li>
+		</ul>
+		<p>If you feel that you can help with any of this (or something else?) do drop in and chat with us. Don't be surprised if nobody answers, since most users in #frokostgruppen are bots anyway.</p>
+	</div>
+</div>
