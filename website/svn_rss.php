1b60f42588906e662ffd0fd5c177fb2de26dab63
Anders Ossowicki <desu@fys.ku.dk>
2008-01-05 01:46:28 +0100

    remove more cruft

--- website/svn_rss.php
+++
@@ -1,9 +0,0 @@
-<?
-$fh = fopen('http://cia.navi.cx/stats/project/anna/.rss', 'r') or die("Failed...");
-$rss_content = "";
-while (!feof($fh)) {
-	$rss_content .= fgets($fh, 4096);
-}
-print $rss_content;
-
-?>
